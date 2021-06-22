26f088aaa48aab5946daab6ec3887015bf51cb9e
hemanthvhs <hemanthvennelakanti@outlook.com>
2021-06-22 21:30:46 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const EventEmitter = require('events');
 const myEmitter = new EventEmitter();
 
 myEmitter.on('sale', () => {
-    console.log('There is a new sale');
-})
+  console.log('There is a new sale');
+});
 
-myEmitter.on('sale', (name) => {
-    console.log(`Consumer Name: ${name}`);
-})
+myEmitter.on('sale', name => {
+  console.log(`Consumer Name: ${name}`);
+});
 
-myEmitter.emit('sale', "Hemanth Vennelakanti")
+myEmitter.emit('sale', 'Hemanth Vennelakanti');
 // Here we are passing "Hemanth Vennelakanti" as the parameter to the callback function when sale event is emitted
